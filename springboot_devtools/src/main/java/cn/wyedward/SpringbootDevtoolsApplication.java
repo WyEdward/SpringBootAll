1c81490c99082b2aebbaa36a2a9e63b5778343e0
jwy <1240561342@qq.com>
2020-03-01 15:14:28 +0800

    springboot热部署

---
+++ springboot_devtools/src/main/java/cn/wyedward/SpringbootDevtoolsApplication.java
@@ -0,0 +1,14 @@
+package cn.wyedward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringbootDevtoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDevtoolsApplication.class, args);
+	}
+
+}
