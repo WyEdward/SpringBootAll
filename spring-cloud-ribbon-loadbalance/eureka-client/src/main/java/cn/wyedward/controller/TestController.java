abcfbf0f7f3961eb9e4526a23123b47c11f861f3
jwy <1240561342@qq.com>
2020-03-25 21:13:51 +0800

    springcloud ribbon客户端负载均衡

---
+++ spring-cloud-ribbon-loadbalance/eureka-client/src/main/java/cn/wyedward/controller/TestController.java
@@ -0,0 +1,32 @@
+package cn.wyedward.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+
+  /*  @GetMapping("/info")
+    public String info(){
+        @SuppressWarnings("deprecation")
+        ServiceInstance instance = client.getLocalServiceInstance();
+        String info = "host：" + instance.getHost() + "，service_id：" + instance.getServiceId();
+        log.info(info);
+        return info;
+    }*/
+
+    @GetMapping("/info")
+    public String hello() {
+        return "hello world";
+    }
+
+
+}
