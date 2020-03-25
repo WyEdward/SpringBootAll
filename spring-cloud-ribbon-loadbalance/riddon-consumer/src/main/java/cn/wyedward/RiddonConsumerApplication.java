e9d25f14a0eca73eff67ab5f626ce0bb9c2651fb
jwy <1240561342@qq.com>
2020-03-25 21:01:15 +0800

    spring cloud ribbon客户端负载均衡

---
+++ spring-cloud-ribbon-loadbalance/riddon-consumer/src/main/java/cn/wyedward/RiddonConsumerApplication.java
@@ -0,0 +1,25 @@
+package cn.wyedward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class RiddonConsumerApplication {
+
+	@Bean
+	@LoadBalanced
+	RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiddonConsumerApplication.class, args);
+	}
+
+}
