858fcea085c16c7c7fcec784375cf6b56da7ccde
jwy <1240561342@qq.com>
2020-05-09 16:55:05 +0800

    springcloud Ribbon客户端负载均衡

--- spring-cloud-hystrix-circuit-breaker/ribbon-consumer-demo3/src/main/java/cn/wyedward/Controller/TestController.java
+++ spring-cloud-hystrix-circuit-breaker/ribbon-consumer-demo3/src/main/java/cn/wyedward/Controller/TestController.java
@@ -1,64 +1,64 @@
-package cn.wyedward.Controller;
+package cn.wyedward.controller;
 
 import cn.wyedward.Service.UserService;
 import cn.wyedward.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 @RestController
 public class TestController {
 
     @Autowired
-    private RestTemplate restTemplate;
-
-    @Autowired
     private UserService userService;
 
-    @GetMapping("user/{id:\\d+}")
-    public User getUser(@PathVariable Long id) {
-        /*Map<String, Object> params = new HashMap<>();
-        params.put("id", id);
-        URI uri = UriComponentsBuilder.fromUriString("http://Server-Provider/user/{id}")
-                .build().expand(params).encode().toUri();
-        return this.restTemplate.getForEntity(uri, User.class).getBody();*/
-        return userService.getUser(id);
+    @GetMapping("testRequestMerge")
+    public void testRequerstMerge() throws InterruptedException, ExecutionException {
+        Future<User> f1 = userService.findUser(1L);
+        Future<User> f2 = userService.findUser(2L);
+        Future<User> f3 = userService.findUser(3L);
+        f1.get();
+        f2.get();
+        f3.get();
+        Thread.sleep(200);
+        Future<User> f4 = userService.findUser(4L);
+        f4.get();
+    }
 
+    @GetMapping("testCache")
+    public void testCache() {
+        userService.getUser(1L);
+        userService.getUser(1L);
+        userService.getUser(1L);
+    }
+
+    @GetMapping("user/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUser(id);
     }
 
     @GetMapping("user")
     public List<User> getUsers() {
-        return this.restTemplate.getForObject("http://Server-Provider/user", List.class);
+        return userService.getUsers();
     }
 
     @GetMapping("user/add")
     public String addUser() {
-        User user = new User(1L, "wyedward", "123456");
-        HttpStatus status = this.restTemplate.postForEntity("http://Server-Provider/user", user, null).getStatusCode();
-        if (status.is2xxSuccessful()) {
-            return "新增用户成功";
-        } else {
-            return "新增用户失败";
-        }
+        return userService.addUser();
     }
 
     @GetMapping("user/update")
     public void updateUser() {
-        User user = new User(1L, "wyedward", "123456");
-        this.restTemplate.put("http://Server-Provider/user", user);
+        userService.updateUser(new User(1L, "mrbird", "123456"));
     }
 
     @GetMapping("user/delete/{id:\\d+}")
     public void deleteUser(@PathVariable Long id) {
-        this.restTemplate.delete("http://Server-Provider/user/{1}", id);
+        userService.deleteUser(id);
     }
 }
