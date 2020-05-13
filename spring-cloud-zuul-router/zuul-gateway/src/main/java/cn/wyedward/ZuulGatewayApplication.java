f7b2d278a5f00c6866477ba91c67abfd2816d8dd
jwy <1240561342@qq.com>
2020-05-13 10:27:26 +0800

    Spring Cloud Zuul服务网关

---
+++ spring-cloud-zuul-router/zuul-gateway/src/main/java/cn/wyedward/ZuulGatewayApplication.java
@@ -0,0 +1,24 @@
+package cn.wyedward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+@RestController
+public class ZuulGatewayApplication {
+
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "hello zuul";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+
+}
