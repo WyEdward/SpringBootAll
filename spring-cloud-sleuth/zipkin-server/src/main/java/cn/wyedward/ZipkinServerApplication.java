a8b85aa86ddd272bd57e46a47fa0b00b58c567cc
jwy <1240561342@qq.com>
2020-05-15 10:13:37 +0800

    使用Spring Cloud Sleuth跟踪微服务

---
+++ spring-cloud-sleuth/zipkin-server/src/main/java/cn/wyedward/ZipkinServerApplication.java
@@ -0,0 +1,17 @@
+package cn.wyedward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+//@EnableZipkinStreamServer
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+
+}
